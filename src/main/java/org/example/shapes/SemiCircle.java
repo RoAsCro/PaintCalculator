34fc962f1b53e5187e388a7bd8515654c5ea14ec
Roland <rollycrompton@gmail.com>
2025-01-15 15:05:39 +0000

    mplemented semi-circle

---
+++ src/main/java/org/example/shapes/SemiCircle.java
@@ -0,0 +1,12 @@
+package org.example.shapes;
+
+import org.example.Shape;
+
+public class SemiCircle extends Shape {
+    private double diameter;
+    public SemiCircle(double[] sides) {
+        super(sides);
+        this.diameter = sides[0];
+        this.area = Math.pow(Math.PI * (this.diameter / 2d), 2d);
+    }
+}
